85fe96db21011d7daa524c80b091b2d5d0dce360
jadecobra <jakeitegsy@yahoo.com>
2022-01-16 00:19:04 -0600

    refactor

--- well_architected/xray_tracer/sns_flow.py
+++ well_architected/xray_tracer/sns_flow.py
@@ -1,3 +1,4 @@
+from typing import ItemsView
 from aws_cdk import (
     aws_lambda as _lambda,
     aws_sns as sns,
@@ -13,17 +14,21 @@ class SnsFlow(core.Stack):
     def __init__(self, scope: core.Construct, id: str, sns_topic: ITopic = None, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        # SNS Topic creation
         topic = sns.Topic(self, 'TheXRayTracerSnsTopic', display_name='The XRay Tracer CDK Pattern Topic')
 
-        sns_lambda = create_python_lambda_function(
+        sns_publisher = self.create_sns_publisher(topic)
+        sns_subscriber = create_python_lambda_function(self, "sns_subscribe")
+
+        topic.grant_publish(sns_publisher)
+        topic.add_subscription(subscriptions.LambdaSubscription(sns_subscriber))
+
+        apigw_topic = sns.Topic.from_topic_arn(self, 'SNSTopic', sns_topic.topic_arn)
+        apigw_topic.add_subscription(subscriptions.LambdaSubscription(sns_publisher))
+
+    def create_sns_publisher(self, topic: ITopic):
+        return create_python_lambda_function(
             self, function_name="sns_publish",
             environment_variables={
                 "TOPIC_ARN": topic.topic_arn
             }
         )
-        topic.grant_publish(sns_lambda)
-        apigw_topic = sns.Topic.from_topic_arn(self, 'SNSTopic', sns_topic.topic_arn)
-        apigw_topic.add_subscription(subscriptions.LambdaSubscription(sns_lambda))
-        sns_subscriber_lambda = create_python_lambda_function(self, "sns_subscribe")
-        topic.add_subscription(subscriptions.LambdaSubscription(sns_subscriber_lambda))
