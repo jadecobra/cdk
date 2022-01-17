ec83adc223355e2294287fb6f50d236a252a0d9b
jadecobra <jakeitegsy@yahoo.com>
2022-01-16 20:52:14 -0600

    update hit_counter

--- well_architected/lambda_functions/hit_counter.py
+++ well_architected/lambda_functions/hit_counter.py
@@ -1,7 +1,16 @@
 import boto3
 import os
 
-dynamodb = boto3.resource('dynamodb')
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.core import patch_all
 
-def handler(event, context=None):
-    return {'statusCode': 200, 'body': 'Hello World'}
+patch_all()
+
+table = boto3.resource('dynamodb').Table(os.environ['HITS_TABLE_NAME'])
+
+def handler(event, context):
+    return table.update_item(
+        Key={'path': event.get('path')},
+        UpdateExpression='ADD hits :incr',
+        ExpressionAttributeValues={':incr': 1}
+    )
