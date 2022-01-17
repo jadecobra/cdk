3ece9a059cc98411dd75bc49fdda412fcb557ccd
jadecobra <jakeitegsy@yahoo.com>
2022-01-16 21:53:25 -0600

    update subscriber lambda

--- well_architected/lambda_functions/sns_publish.py
+++ well_architected/lambda_functions/sns_publish.py
@@ -1,6 +1,11 @@
 import boto3
 import os
 
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.core import patch_all
+
+patch_all()
+
 sns = boto3.client('sns', region_name='us-east-1')
 
 def handler(event, context):
