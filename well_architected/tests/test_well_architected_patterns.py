b9c10c7f71d0db4beb65d0bf1c6dc6a62c07b773
jadecobra <jakeitegsy@yahoo.com>
2022-02-13 08:56:40 -1000

    refactor tests

--- well_architected/tests/test_well_architected_patterns.py
+++ well_architected/tests/test_well_architected_patterns.py
@@ -2,62 +2,39 @@ from tests.utilities import TestTemplates, true, false
 
 class TestWellArchitectedPatterns(TestTemplates):
 
-    def test_dynamodb_flow(self):
-        self.assert_template_equal('DynamoDBFlow')
-
-    def test_dynamodb_table(self):
-        self.assert_template_equal(
+    def patterns(self):
+        return (
+            'DynamoDBFlow',
             'DynamoDBTable',
-        )
-
-    def test_event_bridge_circuit_breaker(self):
-        self.assert_template_equal(
             'EventBridgeCircuitBreaker',
+            'LambdaHttpApiGateway',
+            'HttpFlow',
+            'LambdaFunction',
+            'LambdaRestAPIGateway',
+            'SnsFlow',
+            'SnsRestApi',
+            'SNSTopic',
+            'SqsFlow',
+            'WebApplicationFirewall',
         )
 
-    def test_http_api_gateway(self):
-        self.assert_template_equal(
-            'LambdaHttpApiGateway'
-        )
+    def test_well_architected_cdk_patterns(self):
+        self.assertFalse(True)
+        # for pattern in self.patterns():
+        #     with self.subTest(i=pattern):
+        #         self.assert_template_equal(pattern)
 
-    def test_http_flow(self):
-        self.assert_template_equal(
-            'HttpFlow'
-        )
 
-    def test_lambda_function(self):
-        self.assert_template_equal(
-            'LambdaFunction'
-        )
-
-    def test_rest_api_gateway(self):
-        self.assert_template_equal(
-            'LambdaRestAPIGateway'
-        )
-
-    def test_sns_flow(self):
-        self.assert_template_equal(
-            'SnsFlow'
-        )
-
-    def test_sns_rest_api(self):
-        self.assert_template_equal(
-            'SnsRestApi'
-        )
-
-    def test_sns_topic(self):
-        self.assert_template_equal(
-            'SNSTopic'
-        )
-
-    def test_sqs_flow(self):
-        self.assert_template_equal(
-            'SqsFlow'
-        )
-
-    def test_web_application_firewall(self):
-        self.assert_template_equal(
-            'WebApplicationFirewall'
-        )
-
-    
+    # def test_well_architected_patterns(self):
+    #     self.assert_template_equal('DynamoDBFlow')
+    #     self.assert_template_equal('DynamoDBTable')
+    #     self.assert_template_equal('EventBridgeCircuitBreaker')
+    #     self.assert_template_equal('LambdaHttpApiGateway')
+    #     self.assert_template_equal('HttpFlow')
+    #     self.assert_template_equal('LambdaFunction')
+    #     self.assert_template_equal('LambdaRestAPIGateway')
+    #     self.assert_template_equal('SnsFlow')
+    #     self.assert_template_equal('SnsRestApi')
+    #     self.assert_template_equal('SNSTopic')
+    #     self.assert_template_equal('SqsFlow')
+    #     self.assert_template_equal('WebApplicationFirewall')
