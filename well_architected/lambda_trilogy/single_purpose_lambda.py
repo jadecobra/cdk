ccc00a69570fd7272520e4e8cc69dde3a263ebe7
jadecobra <jakeitegsy@yahoo.com>
2022-02-14 20:35:36 -0800

    refactor stack

--- well_architected/lambda_trilogy/single_purpose_lambda.py
+++ well_architected/lambda_trilogy/single_purpose_lambda.py
@@ -1,6 +1,6 @@
 from aws_cdk import (
     aws_lambda as aws_lambda,
-    aws_apigateway as api_gw,
+    aws_apigateway as api_gateway,
     core as cdk
 )
 
@@ -10,48 +10,28 @@ class TheSinglePurposeFunctionStack(cdk.Stack):
     def __init__(self, scope: cdk.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        ###
-        # We have 3 separate lambda functions, all coming from separate files
-        ###
+        adder = self.create_lambda_function('add')
+        subtracter = self.create_lambda_function('subtract')
+        multiplier = self.create_lambda_function('multiply')
 
-        add_lambda = aws_lambda.Function(
-            self, "addLambdaHandler",
-            runtime=aws_lambda.Runtime.PYTHON_3_8,
-            handler="add.handler",
-            code=aws_lambda.Code.from_asset("lambda_functions/single_purpose_lambda")
-        )
-
-        subtract_lambda = aws_lambda.Function(
-            self, "subtractLambdaHandler",
-            runtime=aws_lambda.Runtime.PYTHON_3_8,
-            handler="subtract.handler",
-            code=aws_lambda.Code.from_asset("lambda_functions/single_purpose_lambda")
-        )
-
-        multiply_lambda = aws_lambda.Function(
-            self, "multiplyLambdaHandler",
-            runtime=aws_lambda.Runtime.PYTHON_3_8,
-            handler="multiply.handler",
-            code=aws_lambda.Code.from_asset("lambda_functions/single_purpose_lambda")
-        )
-
-        ###
-        # All functions have their own endpoint defined on our gateway
-        ##
-
-        api = api_gw.LambdaRestApi(
-            self, 'singlePurposeFunctionAPI',
-            handler=add_lambda,
+        rest_api = api_gateway.LambdaRestApi(
+            self, 'RestAPI',
+            handler=adder,
             proxy=False
         )
 
-        api.root.resource_for_path('add').add_method('GET', api_gw.LambdaIntegration(add_lambda))
-        api.root.resource_for_path('subtract').add_method('GET', api_gw.LambdaIntegration(subtract_lambda))
-        api.root.resource_for_path('multiply').add_method('GET', api_gw.LambdaIntegration(multiply_lambda))
+        for path, lambda_function in {
+            'add': adder,
+            'subtract': subtracter,
+            'multiply': multiplier,
+        }.items():
+            rest_api.root.resource_for_path(path).add_method(
+                'GET', api_gateway.LambdaIntegration(lambda_function)
+            )
 
-    def create_lambda_function(self, logical_name=None, function_name=None):
+    def create_lambda_function(self, function_name):
         return aws_lambda.Function(
-            self, logical_name,
+            self, function_name,
             runtime=aws_lambda.Runtime.PYTHON_3_8,
             handler=f"{function_name}.handler",
             code=aws_lambda.Code.from_asset("lambda_functions/single_purpose_lambda")
