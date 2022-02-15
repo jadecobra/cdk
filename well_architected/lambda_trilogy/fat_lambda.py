9991cad8c1bae1127af2378eac897c931c8617e0
jadecobra <jakeitegsy@yahoo.com>
2022-02-14 19:33:51 -0800

    extract lambda function creation

--- well_architected/lambda_trilogy/fat_lambda.py
+++ well_architected/lambda_trilogy/fat_lambda.py
@@ -1,65 +1,39 @@
 from aws_cdk import (
-    aws_lambda as _lambda,
-    aws_apigateway as api_gw,
-    core
+    aws_lambda as aws_lambda,
+    aws_apigateway as api_gateway,
+    core as cdk
 )
 
+import lambda_function
 
-class TheFatLambdaStack(core.Stack):
+class TheFatLambdaStack(cdk.Stack):
 
-    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+    def __init__(self, scope: cdk.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        ###
-        # Even though all logic lives in the same file, we have 3 separate lambda functions
-        ###
+        adder = self.create_lambda_function('add')
+        subtracter = self.create_lambda_function('subtract')
+        multiplier = self.create_lambda_function('multiply')
 
-        add_lambda = _lambda.Function(
-            self, "addLambdaHandler",
-            runtime=_lambda.Runtime.PYTHON_3_8,
-            handler="fatlambda.add",
-            code=_lambda.Code.from_asset("lambda_functions/fat_lambda")
-        )
-
-        subtract_lambda = _lambda.Function(
-            self, "subtractLambdaHandler",
-            runtime=_lambda.Runtime.PYTHON_3_8,
-            handler="fatlambda.subtract",
-            code=_lambda.Code.from_asset("lambda_functions/fat_lambda")
-        )
-
-        multiply_lambda = _lambda.Function(
-            self, "multiplyLambdaHandler",
-            runtime=_lambda.Runtime.PYTHON_3_8,
-            handler="fatlambda.multiply",
-            code=_lambda.Code.from_asset("lambda_functions/fat_lambda")
-        )
-
-        ###
-        # All functions have their own endpoint defined on our gateway
-        ##
-
-        api = api_gw.LambdaRestApi(
+        rest_api = api_gateway.LambdaRestApi(
             self, 'fatLambdaAPI',
-            handler=add_lambda,
+            handler=adder,
             proxy=False
         )
 
-        # api.root.resource_for_path('add').add_method(
-        #     'GET', api_gw.LambdaIntegration(add_lambda)
-        # )
-        # api.root.resource_for_path('subtract').add_method(
-        #     'GET', api_gw.LambdaIntegration(subtract_lambda)
-        # )
-        # api.root.resource_for_path('multiply').add_method(
-        #     'GET', api_gw.LambdaIntegration(multiply_lambda)
-        # )
-
         for path, lambda_function in {
-            'add': add_lambda,
-            'subtract': subtract_lambda,
-            'multiply': multiply_lambda,
+            'add': adder,
+            'subtract': subtracter,
+            'multiply': multiplier,
         }.items():
-            api.root.resource_for_path(path).add_method(
-                'GET', api_gw.LambdaIntegration(lambda_function)
+            rest_api.root.resource_for_path(path).add_method(
+                'GET', api_gateway.LambdaIntegration(lambda_function)
             )
+
+    def create_lambda_function(self, function_name):
+        return aws_lambda.Function(
+            self, function_name,
+            runtime=aws_lambda.Runtime.PYTHON_3_8,
+            handler=f"fatlambda.{function_name}",
+            code=aws_lambda.Code.from_asset("lambda_functions/fat_lambda")
+        )
