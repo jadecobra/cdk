c3c52effe7a0823e193d58ee1b011d41bed05ae5
jadecobra <jakeitegsy@yahoo.com>
2022-02-23 02:24:04 -0600

    refactor lambda function

--- well_architected/scalable_webhook.py
+++ well_architected/scalable_webhook.py
@@ -14,15 +14,16 @@ class ScalableWebhook(cdk.Stack):
     def __init__(self, scope: cdk.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        # DynamoDB Table
-        # This is standing in for what is RDS on the diagram due to simpler/cheaper setup
-        table = dynamo_db.Table(
+        # This could be any database table. Using dynamodb because it's cheaper
+        database = dynamo_db.Table(
             self, "Messages",
             partition_key=dynamo_db.Attribute(name="id", type=dynamo_db.AttributeType.STRING)
         )
 
-        # Queue Setup
-        queue = sqs.Queue(self, 'RDSPublishQueue', visibility_timeout=cdk.Duration.seconds(300))
+        queue = sqs.Queue(
+            self, 'RDSPublishQueue',
+            visibility_timeout=cdk.Duration.seconds(300)
+        )
 
         publisher = self.create_lambda_function(
             stack_name="SQSPublishLambdaHandler",
@@ -38,13 +39,13 @@ class ScalableWebhook(cdk.Stack):
             function_name='subscribe',
             environment_variables={
                 'queueURL': queue.queue_url,
-                'tableName': table.table_name
+                'tableName': database.table_name
             },
             concurrent_executions=2
         )
         queue.grant_consume_messages(subscriber)
         subscriber.add_event_source(lambda_event.SqsEventSource(queue))
-        table.grant_read_write_data(subscriber)
+        database.grant_read_write_data(subscriber)
 
         api_gw.LambdaRestApi(
             self, 'Endpoint',
@@ -53,7 +54,7 @@ class ScalableWebhook(cdk.Stack):
 
     def create_lambda_function(self, stack_name=None, environment_variables=None, function_name=None, concurrent_executions=None):
         return aws_lambda.Function(
-            self, stack_name,
+            self, function_name,
             runtime=aws_lambda.Runtime.NODEJS_12_X,
             handler="lambda.handler",
             code=aws_lambda.Code.from_asset(f"lambda_functions/{function_name}"),
