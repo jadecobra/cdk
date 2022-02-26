a3421de39fd90665f9593b1ea4d32cd78c02ba80
jadecobra <jakeitegsy@yahoo.com>
2022-02-26 17:01:08 -0600

    refactor lambda functions to python

--- well_architected/lambda_functions/destined_lambda/success.py
+++ well_architected/lambda_functions/destined_lambda/success.py
@@ -1,5 +1,2 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.handler = async (event) => {
-    console.log(JSON.stringify(event, null, 2));
-};
+def handler(event, context):
+    print(event)
