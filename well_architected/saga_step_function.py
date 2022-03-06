98852797f25bd50e3aa5906e2a9b0e5e596fa0df
jadecobra <jakeitegsy@yahoo.com>
2022-03-06 09:15:35 -0600

    format

--- well_architected/saga_step_function.py
+++ well_architected/saga_step_function.py
@@ -3,7 +3,7 @@ from aws_cdk import (
     aws_apigateway as api_gw,
     aws_dynamodb as dynamo_db,
     aws_stepfunctions as step_fn,
-    aws_stepfunctions_tasks as step_fn_tasks,
+    aws_stepfunctions_tabooking_types as step_fn_tabooking_types,
     core as cdk
 )
 
@@ -19,12 +19,13 @@ class SagaStepFunction(cdk.Stack):
         # We store Flight, Hotel and Rental Car bookings in the same table.
         #
         # For more help with single table DB structures see - https://www.dynamodbbook.com/
-        # pk - the trip_id e.g. 1234
-        # sk - bookingtype#booking_id e.g. HOTEL#345634, FLIGHT#574576, PAYMENT#45245
-        table = dynamo_db.Table(self, "Bookings",
-                                partition_key=dynamo_db.Attribute(name="pk", type=dynamo_db.AttributeType.STRING),
-                                sort_key=dynamo_db.Attribute(name="sk", type=dynamo_db.AttributeType.STRING)
-                                )
+        # trip_id - the trip_id e.g. 1234
+        # booking_type - bookingtype#booking_id e.g. HOTEL#345634, FLIGHT#574576, PAYMENT#45245
+        bookings = dynamo_db.Table(
+            self, "Bookings",
+            partition_key=dynamo_db.Attribute(name="trip_id", type=dynamo_db.AttributeType.STRING),
+            sort_key=dynamo_db.Attribute(name="booking_type", type=dynamo_db.AttributeType.STRING)
+        )
 
         ###
         # Lambda Functions
@@ -40,25 +41,25 @@ class SagaStepFunction(cdk.Stack):
 
         # 1) Flights
         reserve_flight_lambda = self.create_lambda(scope=self, lambda_id="reserveFlightLambdaHandler",
-                                                   handler='flights/reserveFlight.handler', table=table)
+                                                   handler='flights/reserveFlight.handler', table=bookings)
         confirm_flight_lambda = self.create_lambda(scope=self, lambda_id="confirmFlightLambdaHandler",
-                                                   handler='flights/confirmFlight.handler', table=table)
+                                                   handler='flights/confirmFlight.handler', table=bookings)
         cancel_flight_lambda = self.create_lambda(scope=self, lambda_id="cancelFlightLambdaHandler",
-                                                  handler='flights/cancelFlight.handler', table=table)
+                                                  handler='flights/cancelFlight.handler', table=bookings)
 
         # 2) Hotel
         reserve_hotel_lambda = self.create_lambda(scope=self, lambda_id="reserveHotelLambdaHandler",
-                                                  handler='hotel/reserveHotel.handler', table=table)
+                                                  handler='hotel/reserveHotel.handler', table=bookings)
         confirm_hotel_lambda = self.create_lambda(scope=self, lambda_id="confirmHotelLambdaHandler",
-                                                  handler='hotel/confirmHotel.handler', table=table)
+                                                  handler='hotel/confirmHotel.handler', table=bookings)
         cancel_hotel_lambda = self.create_lambda(scope=self, lambda_id="cancelHotelLambdaHandler",
-                                                 handler='hotel/cancelHotel.handler', table=table)
+                                                 handler='hotel/cancelHotel.handler', table=bookings)
 
         # 3) Payment For Holiday
         take_payment_lambda = self.create_lambda(scope=self, lambda_id="takePaymentLambdaHandler",
-                                                 handler='payment/takePayment.handler', table=table)
+                                                 handler='payment/takePayment.handler', table=bookings)
         refund_payment_lambda = self.create_lambda(scope=self, lambda_id="refundPaymentLambdaHandler",
-                                                   handler='payment/refundPayment.handler', table=table)
+                                                   handler='payment/refundPayment.handler', table=bookings)
 
         ###
         # Saga Pattern Step Function
@@ -73,51 +74,51 @@ class SagaStepFunction(cdk.Stack):
         booking_failed = step_fn.Fail(self, "Sorry, We Couldn't make the booking")
 
         # 1) Reserve Flights and Hotel
-        cancel_hotel_reservation = step_fn_tasks.LambdaInvoke(
+        cancel_hotel_reservation = step_fn_tabooking_types.LambdaInvoke(
             self, 'CancelHotelReservation',
             lambda_function=cancel_hotel_lambda,
             result_path='$.CancelHotelReservationResult'
         ).add_retry(max_attempts=3).next(booking_failed)
 
-        reserve_hotel = step_fn_tasks.LambdaInvoke(
+        reserve_hotel = step_fn_tabooking_types.LambdaInvoke(
             self, 'ReserveHotel',
             lambda_function=reserve_hotel_lambda,
             result_path='$.ReserveHotelResult'
         ).add_catch(cancel_hotel_reservation, result_path="$.ReserveHotelError")
 
-        cancel_flight_reservation = step_fn_tasks.LambdaInvoke(
+        cancel_flight_reservation = step_fn_tabooking_types.LambdaInvoke(
             self, 'CancelFlightReservation',
             lambda_function=cancel_flight_lambda,
             result_path='$.CancelFlightReservationResult'
         ).add_retry(max_attempts=3).next(cancel_hotel_reservation)
 
-        reserve_flight = step_fn_tasks.LambdaInvoke(
+        reserve_flight = step_fn_tabooking_types.LambdaInvoke(
             self, 'ReserveFlight',
             lambda_function=reserve_flight_lambda,
             result_path='$.ReserveFlightResult'
         ).add_catch(cancel_flight_reservation, result_path="$.ReserveFlightError")
 
         # 2) Take Payment
-        refund_payment = step_fn_tasks.LambdaInvoke(
+        refund_payment = step_fn_tabooking_types.LambdaInvoke(
             self, 'RefundPayment',
             lambda_function=refund_payment_lambda,
             result_path='$.RefundPaymentResult'
         ).add_retry(max_attempts=3).next(cancel_flight_reservation)
 
-        take_payment = step_fn_tasks.LambdaInvoke(
+        take_payment = step_fn_tabooking_types.LambdaInvoke(
             self, 'TakePayment',
             lambda_function=take_payment_lambda,
             result_path='$.TakePaymentResult'
         ).add_catch(refund_payment, result_path="$.TakePaymentError")
 
         # 3) Confirm Flight and Hotel Booking
-        confirm_hotel = step_fn_tasks.LambdaInvoke(
+        confirm_hotel = step_fn_tabooking_types.LambdaInvoke(
             self, 'ConfirmHotelBooking',
             lambda_function=confirm_hotel_lambda,
             result_path='$.ConfirmHotelBookingResult'
         ).add_catch(refund_payment, result_path="$.ConfirmHotelBookingError")
 
-        confirm_flight = step_fn_tasks.LambdaInvoke(
+        confirm_flight = step_fn_tabooking_types.LambdaInvoke(
             self, 'ConfirmFlight',
             lambda_function=confirm_flight_lambda,
             result_path='$.ConfirmFlightResult'
