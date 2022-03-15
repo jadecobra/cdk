d19fd32432a60e47385c1e6d21a06ff1a17be560
jadecobra <jakeitegsy@yahoo.com>
2022-03-14 20:13:56 -0500

    add saga step lambda functions

---
+++ lambda_functions/hotels/confirm_hotel.py
@@ -0,0 +1,38 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const { DynamoDB } = require('aws-sdk');
+exports.handler = async function (event) {
+    console.log("request:", JSON.stringify(event, undefined, 2));
+    let bookingID = '';
+    // If we passed the parameter to fail this step
+    if (event.run_type === 'failHotelConfirmation') {
+        throw new Error('Failed to confirm the hotel booking');
+    }
+    if (typeof event.ReserveHotelResult !== 'undefined') {
+        bookingID = event.ReserveHotelResult.Payload.booking_id;
+    }
+    // create AWS SDK clients
+    const dynamo = new DynamoDB();
+    var params = {
+        TableName: process.env.TABLE_NAME,
+        Key: {
+            'pk': { S: event.trip_id },
+            'sk': { S: 'HOTEL#' + bookingID }
+        },
+        "UpdateExpression": "set transaction_status = :booked",
+        "ExpressionAttributeValues": {
+            ":booked": { "S": "confirmed" }
+        }
+    };
+    // Call DynamoDB to add the item to the table
+    let result = await dynamo.updateItem(params).promise().catch((error) => {
+        throw new Error(error);
+    });
+    console.log('updated hotel booking:');
+    console.log(result);
+    // return status of ok
+    return {
+        status: "ok",
+        booking_id: bookingID
+    };
+};
