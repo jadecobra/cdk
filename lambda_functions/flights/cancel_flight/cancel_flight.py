81896f3efc8dc376c050ec9aa86afb3037d54a07
jadecobra <jakeitegsy@yahoo.com>
2022-03-14 21:33:51 -0500

    refactor flights lambda functions

--- lambda_functions/flights/cancel_flight/cancel_flight.py
+++ lambda_functions/flights/cancel_flight/cancel_flight.py
@@ -6,34 +6,16 @@ TABLE = boto3.resource('dynamodb').Table(os.environ.get('TABLE_NAME'))
 
 def handler(event, context):
     print(f"request: {event}")
-    
-    pass
-
-Object.defineProperty(exports, "__esModule", { value: true });
-
-exports.handler = async function (event) {
-    if (Math.random() < 0.4) {
-        throw new Error("Internal Server Error");
-    }
-    let bookingID = '';
-    if (typeof event.ReserveFlightResult !== 'undefined') {
-        bookingID = event.ReserveFlightResult.Payload.booking_id;
-    }
-    // create AWS SDK clients
-    const dynamo = new DynamoDB();
-    var params = {
-        TableName: process.env.TABLE_NAME,
-        Key: {
-            'pk': { S: event.trip_id },
-            'sk': { S: 'FLIGHT#' + bookingID }
+    if random.random() < 0.4:
+        raise Exception('Internal Server Error')
+    booking_id = event['ReserveFlightResult']['Payload']['booking_id']
+    result = TABLE.delete_item(
+        Key={
+            'booking_id': { event.get('trip_id') },
+            'booking_type': { f'FLIGHT#{event.get(booking_id)}'}
         }
-    };
-    // Call DynamoDB to add the item to the table
-    let result = await dynamo.deleteItem(params).promise().catch((error) => {
-        throw new Error(error);
-    });
-    console.log('deleted flight booking:');
-    console.log(result);
-    // return status of ok
-    return { status: "ok" };
-};
+    )
+
+    print('deleted flight booking:')
+    print(result)
+    return { 'status': "ok" }
