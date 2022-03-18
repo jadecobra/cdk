09cc8a429a17e6b98cf7fc5b04780d61b851546f
jadecobra <jakeitegsy@yahoo.com>
2022-03-18 02:41:22 -0500

    move templates

--- tests/utilities.py
+++ tests/utilities.py
@@ -33,5 +33,5 @@ class TestTemplates(unittest.TestCase):
         os.system('clear')
         time_it(os.system, f'cdk ls {stack_name} --version-reporting=false --path-metadata=false --asset-metadata=false', description=f'synthesize stack: {stack_name}')
         with open(f'cdk.out/{stack_name}.template.json') as template:
-            with open(f'tests/{stack_name}.template.json') as fixture:
+            with open(f'tests/templates/{stack_name}.template.json') as fixture:
                 return self.assertEqual(json.load(template), json.load(fixture))
