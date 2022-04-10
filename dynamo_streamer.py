271e4b49caa92ffebc034326054c05f3efca064f
jadecobra <jakeitegsy@yahoo.com>
2022-04-10 14:48:09 -0500

    rename logical ids

--- dynamo_streamer.py
+++ dynamo_streamer.py
@@ -16,64 +16,85 @@ class DynamoStreamer(core.Stack):
 
         # DynamoDB Table
         # Streaming is enabled to send the whole new object down the pipe
-        table = dynamo_db.Table(self, "TheDynamoStreamer",
-                                partition_key=dynamo_db.Attribute(name="message",
-                                                                  type=dynamo_db.AttributeType.STRING),
-                                stream=dynamo_db.StreamViewType.NEW_IMAGE
-                                )
+        dynamodb_table = dynamo_db.Table(
+            self, "DynamoDbTable",
+            partition_key=dynamo_db.Attribute(
+                name="message",
+                type=dynamo_db.AttributeType.STRING
+            ),
+            stream=dynamo_db.StreamViewType.NEW_IMAGE
+        )
 
         # defines an AWS  Lambda resource
-        subscriber_lambda = _lambda.Function(self, "DynamoLambdaHandler",
-                                             runtime=_lambda.Runtime.PYTHON_3_8,
-                                             handler="lambda.handler",
-                                             code=_lambda.Code.from_asset("lambda_fns/subscribe")
-                                             )
+        subscriber_lambda = _lambda.Function(
+            self, 'LambdaFunction',
+            runtime=_lambda.Runtime.PYTHON_3_8,
+            handler="lambda.handler",
+            code=_lambda.Code.from_asset("lambda_functions/subscribe")
+        )
 
-        subscriber_lambda.add_event_source(_event.DynamoEventSource(table=table,
-                                                                    starting_position=_lambda.StartingPosition.LATEST))
+        subscriber_lambda.add_event_source(
+            _event.DynamoEventSource(
+                table=dynamodb_table,
+                starting_position=_lambda.StartingPosition.LATEST
+            )
+        )
 
         # API Gateway Creation
-        gateway = api_gw.RestApi(self, 'DynamoStreamerAPI',
-                                 deploy_options=api_gw.StageOptions(metrics_enabled=True,
-                                                                    logging_level=api_gw.MethodLoggingLevel.INFO,
-                                                                    data_trace_enabled=True,
-                                                                    stage_name='prod'
-                                                                    ))
+        api_gateway = api_gw.RestApi(
+            self, 'ApiGateway',
+            deploy_options=api_gw.StageOptions(
+                metrics_enabled=True,
+                logging_level=api_gw.MethodLoggingLevel.INFO,
+                data_trace_enabled=True,
+                stage_name='prod'
+            )
+        )
 
         # Give our gateway permissions to interact with dynamodb
-        api_gw_dynamo_role = iam.Role(self, 'DefaultLambdaHanderRole',
-                                      assumed_by=iam.ServicePrincipal('apigateway.amazonaws.com'))
-        table.grant_read_write_data(api_gw_dynamo_role)
+        api_gw_dynamo_role = iam.Role(
+            self, 'ApiGatewayRole',
+            assumed_by=iam.ServicePrincipal('apigateway.amazonaws.com')
+        )
+        dynamodb_table.grant_read_write_data(api_gw_dynamo_role)
 
         # shortening the lines of later code
         schema = api_gw.JsonSchema
         schema_type = api_gw.JsonSchemaType
 
         # Because this isn't a proxy integration, we need to define our response model
-        response_model = gateway.add_model('ResponseModel',
-                                           content_type='application/json',
-                                           model_name='ResponseModel',
-                                           schema=schema(schema=api_gw.JsonSchemaVersion.DRAFT4,
-                                                         title='pollResponse',
-                                                         type=schema_type.OBJECT,
-                                                         properties={
-                                                             'message': schema(type=schema_type.STRING)
-                                                         }))
+        response_model = api_gateway.add_model(
+            'ResponseModel',
+            content_type='application/json',
+            model_name='ResponseModel',
+            schema=schema(
+                schema=api_gw.JsonSchemaVersion.DRAFT4,
+                title='pollResponse',
+                type=schema_type.OBJECT,
+                properties={
+                    'message': schema(type=schema_type.STRING)
+                }
+            )
+        )
 
-        error_response_model = gateway.add_model('ErrorResponseModel',
-                                                 content_type='application/json',
-                                                 model_name='ErrorResponseModel',
-                                                 schema=schema(schema=api_gw.JsonSchemaVersion.DRAFT4,
-                                                               title='errorResponse',
-                                                               type=schema_type.OBJECT,
-                                                               properties={
-                                                                   'state': schema(type=schema_type.STRING),
-                                                                   'message': schema(type=schema_type.STRING)
-                                                               }))
+        error_response_model = api_gateway.add_model(
+            'ErrorResponseModel',
+            content_type='application/json',
+            model_name='ErrorResponseModel',
+            schema=schema(
+                schema=api_gw.JsonSchemaVersion.DRAFT4,
+                title='errorResponse',
+                type=schema_type.OBJECT,
+                properties={
+                    'state': schema(type=schema_type.STRING),
+                    'message': schema(type=schema_type.STRING)
+                }
+            )
+        )
 
         # This is the VTL to transform our incoming JSON to a Dynamo Insert Query
         request_template = {
-            "TableName": table.table_name,
+            "TableName": dynamodb_table.table_name,
             "Item": {
                 "message": {"S": "$input.path('$.message')"}
             }
@@ -117,30 +138,39 @@ class DynamoStreamer(core.Stack):
         )
 
         # Add an InsertItem endpoint onto the gateway
-        gateway.root.add_resource('InsertItem') \
-            .add_method('POST', api_gw.Integration(type=api_gw.IntegrationType.AWS,
-                                                   integration_http_method='POST',
-                                                   uri='arn:aws:apigateway:us-east-1:dynamodb:action/PutItem',
-                                                   options=integration_options
-                                                   ),
-                        method_responses=[
-                            api_gw.MethodResponse(status_code='200',
-                                                  response_parameters={
-                                                      'method.response.header.Content-Type': True,
-                                                      'method.response.header.Access-Control-Allow-Origin': True,
-                                                      'method.response.header.Access-Control-Allow-Credentials': True
-                                                  },
-                                                  response_models={
-                                                      'application/json': response_model
-                                                  }),
-                            api_gw.MethodResponse(status_code='400',
-                                                  response_parameters={
-                                                      'method.response.header.Content-Type': True,
-                                                      'method.response.header.Access-Control-Allow-Origin': True,
-                                                      'method.response.header.Access-Control-Allow-Credentials': True
-                                                  },
-                                                  response_models={
-                                                      'application/json': error_response_model
-                                                  }),
-                        ]
-                        )
+        (
+            api_gateway.root
+            .add_resource('InsertItem')
+            .add_method(
+                'POST', api_gw.Integration(
+                    type=api_gw.IntegrationType.AWS,
+                    integration_http_method='POST',
+                    uri='arn:aws:apigateway:us-east-1:dynamodb:action/PutItem',
+                    options=integration_options
+                ),
+                method_responses=[
+                    api_gw.MethodResponse(
+                        status_code='200',
+                        response_parameters={
+                            'method.response.header.Content-Type': True,
+                            'method.response.header.Access-Control-Allow-Origin': True,
+                            'method.response.header.Access-Control-Allow-Credentials': True
+                        },
+                        response_models={
+                            'application/json': response_model
+                        }
+                    ),
+                    api_gw.MethodResponse(
+                        status_code='400',
+                        response_parameters={
+                            'method.response.header.Content-Type': True,
+                            'method.response.header.Access-Control-Allow-Origin': True,
+                            'method.response.header.Access-Control-Allow-Credentials': True
+                        },
+                        response_models={
+                            'application/json': error_response_model
+                        }
+                    ),
+                ]
+            )
+        )
