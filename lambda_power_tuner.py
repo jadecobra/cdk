e40cdc9f595ea26c842b71b1398e5b77f657285c
jadecobra <jakeitegsy@yahoo.com>
2022-04-11 21:15:20 -0500

    reorganize code

--- lambda_power_tuner.py
+++ lambda_power_tuner.py
@@ -1,3 +1,6 @@
+import aws_cdk.core
+import aws_cdk.aws_lambda
+import aws_cdk.aws_sam
 from aws_cdk import (
     aws_lambda as _lambda,
     aws_sam as sam,
@@ -7,26 +10,21 @@ from aws_cdk import (
 
 class LambdaPowerTuner(core.Stack):
 
-    
-
-    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+    def __init__(self, scope: aws_cdk.core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        power_values = '128,256,512,1024,1536,3008'
-        lambda_resource = '*'
-
-        # An example lambda that can be used to test the powertuner
-        example_lambda = _lambda.Function(self, "exampleLambda",
-                                          runtime=_lambda.Runtime.NODEJS_12_X,
-                                          handler="index.handler",
-                                          code=_lambda.Code.from_inline("exports.handler = function(event, ctx, cb) { return cb(null, 'hi'); }"),
-                                          )
+        example_lambda_function = aws_cdk.aws_lambda.Function(
+            self, "exampleLambda",
+            runtime=aws_cdk.aws_lambda.Runtime.NODEJS_12_X,
+            handler="index.handler",
+            code=aws_cdk.aws_lambda.Code.from_inline("exports.handler = function(event, ctx, cb) { return cb(null, 'hi'); }"),
+        )
 
         # uncomment to only allow this power tuner to manipulate this defined function
-        # lambda_resource = example_lambda.function_arn
+        # lambda_resource = example_lamdba.function_arn
 
         # Output the Lambda function ARN in the deploy logs to ease testing
-        core.CfnOutput(self, 'LambdaARN', value=example_lambda.function_arn)
+        core.CfnOutput(self, 'LambdaARN', value=example_lambda_function.function_arn)
 
         # Deploy the aws-lambda-powertuning application from the Serverless Application Repository
         # https://serverlessrepo.aws.amazon.com/applications/arn:aws:serverlessrepo:us-east-1:451282441545:applications~aws-lambda-power-tuning
@@ -34,6 +32,15 @@ class LambdaPowerTuner(core.Stack):
             "applicationId": "arn:aws:serverlessrepo:us-east-1:451282441545:applications/aws-lambda-power-tuning",
             "semanticVersion": "3.4.0"
         }, parameters={
-            "lambdaResource": lambda_resource,
-            "PowerValues": power_values
+            "lambdaResource": self.default_lambda_resources(),
+            "PowerValues": self.power_values()
         })
+
+
+    @staticmethod
+    def power_values():
+        return '128,256,512,1024,1536,3008'
+
+    @staticmethod
+    def default_lambda_resources():
+        return '*'
