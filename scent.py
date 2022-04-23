8ccd8b0bb566503feb2c4a391ac64e64495126a0
jadecobra <jakeitegsy@yahoo.com>
2022-04-22 20:52:02 -0500

    update scent

--- scent.py
+++ scent.py
@@ -28,7 +28,7 @@ watch_paths = [
 #     return filename.endswith('.py') and not os.path.basename(filename).startswith('.')
 
 @sniffer.api.file_validator
-def json_files(filename):
+def files(filename):
     return (
         filename.endswith('.py')
     #  or filename.endswith('.json')
