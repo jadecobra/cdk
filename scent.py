ab5d7557875aa116cc4d86653eb7fe4d9694789c
jadecobra <jakeitegsy@yahoo.com>
2022-04-22 20:41:35 -0500

    update tests

--- scent.py
+++ scent.py
@@ -12,16 +12,20 @@ import os
 # fail_bg_color = termstyle.bg_default
 
 # All lists in this variable will be under surveillance for changes.
-watch_paths = ['.', 'tests/', 'cdk.out/']
+watch_paths = [
+    '.',
+    # 'tests/',
+    'cdk.out/'
+]
 
 # this gets invoked on every file that gets changed in the directory. Return
 # True to invoke any runnable functions, False otherwise.
 #
 # This fires runnables only if files ending with .py extension and not prefixed
 # with a period.
-@sniffer.api.file_validator
-def py_files(filename):
-    return filename.endswith('.py') and not os.path.basename(filename).startswith('.')
+# @sniffer.api.file_validator
+# def py_files(filename):
+#     return filename.endswith('.py') and not os.path.basename(filename).startswith('.')
 
 @sniffer.api.file_validator
 def json_files(filename):
