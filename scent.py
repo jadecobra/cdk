60ab591c66e09e3b305e9a6d0c3a99d0b5f567a6
jadecobra <jakeitegsy@yahoo.com>
2022-04-22 20:37:28 -0500

    update scent

--- scent.py
+++ scent.py
@@ -2,18 +2,26 @@ import sniffer.api
 import subprocess
 import os
 
-def filename_endswith(filename=None, suffix=None):
-    return filename.endswith(f'.{suffix}') and not os.path.basename(filename).startswith('.')
+# from sniffer.api import * # import the really small API
+# import os, termstyle
 
+# you can customize the pass/fail colors like this
+# pass_fg_color = termstyle.green
+# pass_bg_color = termstyle.bg_default
+# fail_fg_color = termstyle.red
+# fail_bg_color = termstyle.bg_default
+
+# All lists in this variable will be under surveillance for changes.
+# watch_paths = ['.', 'tests/']
+
+# this gets invoked on every file that gets changed in the directory. Return
+# True to invoke any runnable functions, False otherwise.
+#
+# This fires runnables only if files ending with .py extension and not prefixed
+# with a period.
 @sniffer.api.file_validator
 def py_files(filename):
-    return filename_endswith(filename, 'py')
     return filename.endswith('.py') and not os.path.basename(filename).startswith('.')
-
-@sniffer.api.file_validator
-def json_files(filename):
-    return filename_endswith(filename, 'json')
-    return filename.endswith('.json') and not os.path.basename(filename).startswith('.')
 
 @sniffer.api.runnable
 def run_tests(*args):
