955a99c1ab2ac7e6625001a6fdb12a8b6b9a8415
jadecobra <jakeitegsy@yahoo.com>
2022-04-22 20:28:41 -0500

    update scent

--- scent.py
+++ scent.py
@@ -2,12 +2,17 @@ import sniffer.api
 import subprocess
 import os
 
-@sniffer.api.file_validator
-def py_files(filename):
-    return filename.endswith('.py') and not os.path.basename(filename).startswith('.')
+def filename_endswith(filename=None, suffix=None):
+    return filename.endswith(f'.{suffix}') and not os.path.basename(filename).startswith('.')
 
 @sniffer.api.file_validator
 def py_files(filename):
+    return filename_endswith(filename, 'py')
+    return filename.endswith('.py') and not os.path.basename(filename).startswith('.')
+
+@sniffer.api.file_validator
+def json_files(filename):
+    return filename_endswith(filename, 'json')
     return filename.endswith('.json') and not os.path.basename(filename).startswith('.')
 
 @sniffer.api.runnable
