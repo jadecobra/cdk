d51231700441bc621c9534aaec645e111f997183
jadecobra <jakeitegsy@yahoo.com>
2022-04-23 21:14:11 -0500

    update imports

--- http_api.py
+++ http_api.py
@@ -1,36 +1,43 @@
 import aws_cdk
 import constructs
+import api_gateway_cloudwatch
+import aws_cdk.aws_apigatewayv2_alpha
+import aws_cdk.aws_apigatewayv2_integrations_alpha
 
 from aws_cdk.aws_lambda import Function
-from aws_cdk.aws_apigatewayv2_alpha import HttpApi
-from aws_cdk.aws_apigatewayv2_integrations_alpha import HttpLambdaIntegration
-from api_gateway_cloudwatch import ApiGatewayCloudWatch
-from well_architected import WellArchitectedFrameworkConstruct
+
 
 
 class LambdaHttpApiGateway(aws_cdk.Stack):
 
-    def __init__(self, scope: constructs.Construct, id: str, lambda_function: Function, error_topic=None, **kwargs) -> None:
+    def __init__(
+        self, scope: constructs.Construct, id: str,
+        lambda_function: aws_cdk.aws_lambda.Function,
+        error_topic=None,
+        **kwargs
+    ) -> None:
         super().__init__(scope, id, **kwargs)
 
-        self.http_api = HttpApi(
-            self, 'HttpAPI',
-            default_integration=HttpLambdaIntegration(
-                'HTTPLambdaIntegration',
-                handler=lambda_function
-            )
-        )
-
-        ApiGatewayCloudWatch(
-            self, 'ApiGatewayCloudWatch',
+        self.http_api = self.create_http_api_lambda_integration(lambda_function)
+        api_gateway_cloudwatch.ApiGatewayCloudWatch(
+            self, 'HttpApiGatewayCloudWatch',
             api_id=self.http_api.http_api_id,
             error_topic=error_topic,
         )
 
         aws_cdk.CfnOutput(self, 'HTTP API Url', value=self.http_api.url)
 
+    def create_http_api_lambda_integration(self, lambda_function):
+        return aws_cdk.aws_apigatewayv2_alpha.HttpApi(
+            self, 'HttpApi',
+            default_integration=aws_cdk.aws_apigatewayv2_integrations_alpha.HttpLambdaIntegration(
+                'HTTPLambdaIntegration',
+                handler=lambda_function
+            )
+        )
 
-class HttpApi(WellArchitectedFrameworkConstruct):
+
+class HttpApi(well_architected.WellArchitectedFrameworkConstruct):
 
     def __init__(
         self, scope: constructs.Construct, id: str,
@@ -38,9 +45,12 @@ class HttpApi(WellArchitectedFrameworkConstruct):
     ) -> None:
         super().__init__(scope, id, **kwargs)
 
-        self.http_api = HttpApi(self, 'HttpAPI')
+        self.http_api = aws_cdk.aws_apigatewayv2_alpha.HttpApi(
+            self, 'HttpAPI',
+            create_default_stage=True,
+        )
 
-        ApiGatewayCloudWatch(
+        api_gateway_cloudwatch.ApiGatewayCloudWatch(
             self, 'ApiGatewayCloudWatch',
             api_id=self.http_api.http_api_id,
             error_topic=error_topic,
