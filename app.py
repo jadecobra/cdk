ceb681140f927b54daba6140bb8d5a837d695997
jadecobra <jakeitegsy@yahoo.com>
2022-04-26 21:58:03 -0500

    rename

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ try:
     import event_bridge_etl
     import http_api_dynamodb
     import http_api_step_functions
-    import api_lambda_dynamodb
+    import waf_api_lambda_dynamodb
     import lambda_circuit_breaker
     import lambda_power_tuner
     import lambda_trilogy.lambda_lith
@@ -63,7 +63,7 @@ class WellArchitected(aws_cdk.App):
         # simple_graphql_service.SimpleGraphQlService(self, "SimpleGraphqlService", )
         # dynamo_streamer.DynamoStreamer(self, "DynamoStreamer", )
         # lambda_power_tuner.LambdaPowerTuner(self, "LambdaPowerTuner", )
-        api_lambda_dynamodb.HttpApiLambdaDynamodb(self, 'ApiLambdaDynamodb')
+        waf_api_lambda_dynamodb.WafApiLambdaDynamodb(self, 'WafApiLambdaDynamodb')
         # http_api_step_functions.HttpApiStateMachine(self, "HttpApiStateMachine")
 
     def create_webservice(self):
