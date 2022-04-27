a829135065cf8de89c327d7e5a5edb053bf273a8
jadecobra <jakeitegsy@yahoo.com>
2022-04-26 22:13:36 -0500

    add test

--- tests/test_well_architected_patterns.py
+++ tests/test_well_architected_patterns.py
@@ -33,6 +33,7 @@ class TestWellArchitectedPatterns(tests.utilities.TestTemplates):
             # 'HttpApiStateMachine',
             # 'WebApplicationFirewall',
             # 'XRayTracerSnsFanOutTopic',
+            "ApiStepFunctions",
             'WafApiLambdaDynamodb',
         )
 
