6c22e8826a9a7e5e6d9f9fedeeb48640f0ec2bfb
jadecobra <jakeitegsy@yahoo.com>
2022-05-21 18:19:11 -0500

    add narrative

--- circuit_breaker_lambda.py
+++ circuit_breaker_lambda.py
@@ -12,28 +12,31 @@ import subprocess
 class CircuitBreakerLambda(well_architected.WellArchitectedStack):
 
     def __init__(self, scope: constructs.Construct, id: str, **kwargs) -> None:
-        super().__init__(scope, id, **kwargs)
+        super().__init__(scope, id, **kwargs)\
+        dynamodb_table = self.create_dynamodb_table(id)
+        lambda_function = self.create_lambda_function(dynamodb_table.table_name)
+        dynamodb_table.grant_read_write_data(lambda_function)
+        aws_cdk.CfnOutput(self, 'HTTP API Url', value=self.create_http_api(lambda_function).url)
 
-        dynamodb_table = well_architected_dynamodb_table.DynamoDBTableConstruct(
+    def create_dynamodb_table(self, id):
+        return well_architected_dynamodb_table.DynamoDBTableConstruct(
             self, id,
             partition_key="id",
             error_topic=self.error_topic,
         ).dynamodb_table
 
-        lambda_function = well_architected_lambda.LambdaFunctionConstruct(
+    def create_lambda_function(self, table_name):
+        return well_architected_lambda.LambdaFunctionConstruct(
             self, 'LambdaFunction',
             error_topic=self.error_topic,
             function_name='circuit_breaker_lambda',
             environment_variables={
-                'CIRCUITBREAKER_TABLE': dynamodb_table.table_name
+                'CIRCUITBREAKER_TABLE': table_name
             }
         ).lambda_function
 
-        # grant the lambda role read/write permissions to our table'
-        dynamodb_table.grant_read_write_data(lambda_function)
-
-
-        http_api = well_architected_api.WellArchitectedApi(
+    def create_http_api(self, lambda_function):
+        return well_architected_api.WellArchitectedApi(
             self, 'HttpApiGateway',
             error_topic=self.error_topic,
             api=aws_cdk.aws_apigatewayv2_alpha.HttpApi(
@@ -44,5 +47,3 @@ class CircuitBreakerLambda(well_architected.WellArchitectedStack):
                 ),
             )
         ).api
-
-        aws_cdk.CfnOutput(self, 'HTTP API Url', value=http_api.url)
