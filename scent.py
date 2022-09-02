d2920b8e987751cad4d518677a84356cd460458b
jadecobra <jakeitegsy@yahoo.com>
2022-09-02 00:23:13 -0500

    add tests

--- scent.py
+++ scent.py
@@ -2,34 +2,7 @@ import sniffer.api
 import subprocess
 import os
 
-# from sniffer.api import * # import the really small API
-# import os, termstyle
-
-# you can customize the pass/fail colors like this
-# pass_fg_color = termstyle.green
-# pass_bg_color = termstyle.bg_default
-# fail_fg_color = termstyle.red
-# fail_bg_color = termstyle.bg_default
-
-# All lists in this variable will be under surveillance for changes.
 watch_paths = ['tests/', 'src/']
-
-
-# this gets invoked on every file that gets changed in the directory. Return
-# True to invoke any runnable functions, False otherwise.
-#
-# This fires runnables only if files ending with .py extension and not prefixed
-# with a period.
-# @sniffer.api.file_validator
-# def py_files(filename):
-#     return filename.endswith('.py') and not os.path.basename(filename).startswith('.')
-
-# @sniffer.api.file_validator
-# def is_valid(filename):
-#     return (
-#         filename.endswith('.py')
-#      or filename.endswith('.json')
-#     ) and not os.path.basename(filename).startswith('.')
 
 @sniffer.api.runnable
 def run_tests(*args):
--- tests/test_z_build_publish.py
+++
@@ -1,7 +0,0 @@
-import jadecobra.toolkit
-
-
-class TestBuildDeploy(jadecobra.toolkit.TestCase):
-
-    def test_build_and_publish(self):
-        self.build_and_publish()
