2d131391fdc1e9632c46908e78ca46d9dc80eef4
jadecobra <jakeitegsy@yahoo.com>
2022-09-12 15:43:50 -0500

    refactor sns_lambda_dynamodb

--- well_architected_stacks/src/well_architected_stacks/sns_lambda.py
+++ well_architected_stacks/src/well_architected_stacks/sns_lambda.py
@@ -11,12 +11,13 @@ class SnsLambda(well_architected_stack.Stack):
         self, scope: constructs.Construct,
         id: str,
         sns_topic: aws_cdk.aws_sns.ITopic=None,
+        lambda_function_name=None,
         **kwargs
     ) -> None:
         super().__init__(scope, id, **kwargs)
         well_architected_constructs.sns_lambda.SnsLambdaConstruct(
             self, "SnsLambda",
-            function_name="sns_lambda",
+            function_name=lambda_function_name,
             lambda_directory=self.lambda_directory,
             sns_topic=sns_topic,
             error_topic=self.error_topic,
--- well_architected_stacks/src/well_architected_stacks/sns_lambda_dynamodb.py
+++ well_architected_stacks/src/well_architected_stacks/sns_lambda_dynamodb.py
@@ -10,29 +10,27 @@ class SnsLambdaDynamodb(well_architected_stack.Stack):
     def __init__(
         self, scope: constructs.Construct, id: str,
         sns_topic=None,
+        partition_key=None,
+        lambda_function_name=None,
         **kwargs
     ) -> None:
         super().__init__(scope, id, **kwargs)
 
-
         self.dynamodb_table = well_architected_constructs.dynamodb_table.DynamodbTableConstruct(
             self, "DynamoDbTable",
-            partition_key="path",
+            partition_key=partition_key,
             error_topic=self.error_topic,
         ).dynamodb_table
 
-        self.lambda_function = well_architected_constructs.lambda_function.create_python_lambda_function(
-            self, function_name="hit_counter",
+        self.lambda_function = well_architected_constructs.sns_lambda.SnsLambdaConstruct(
+            self, 'SnsLambda',
+            function_name=lambda_function_name,
+            sns_topic=sns_topic,
             error_topic=self.error_topic,
             lambda_directory=self.lambda_directory,
             environment_variables={
                 "HITS_TABLE_NAME": self.dynamodb_table.table_name
             }
-        )
+        ).lambda_function
 
         self.dynamodb_table.grant_read_write_data(self.lambda_function)
-        sns_topic.add_subscription(
-            aws_cdk.aws_sns_subscriptions.LambdaSubscription(
-                self.lambda_function
-            )
-        )
