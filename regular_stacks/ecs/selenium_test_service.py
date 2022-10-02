4e2a58babf5967d58e5dd4dbe4e4dc7a94798c62
jadecobra <jakeitegsy@yahoo.com>
2022-10-02 12:14:16 -0500

    add container definitions

--- regular_stacks/ecs/selenium_test_service.py
+++ regular_stacks/ecs/selenium_test_service.py
@@ -10,7 +10,6 @@ class SeleniumTestService(well_architected_stacks.well_architected_stack.Stack):
         self, scope: constructs.Construct, construct_id: str,
         name=None,
         container_image=None,
-        container_name=None,
         max_capacity=None,
         **kwargs
     ) -> None:
@@ -18,19 +17,44 @@ class SeleniumTestService(well_architected_stacks.well_architected_stack.Stack):
         self.name = name
         self.vpc = self.get_vpc()
         self.ecs_cluster = self.create_autoscaling_ecs_cluster()
+        # self.ecs_cluster.connections.allow_from_any_ipv4(
+        #     port_range=aws_cdk.aws_ec2.Port.tcp(4444),
+        #     description="Port 5555 for inbound traffic",
+
+        # )
+        self.ecs_cluster.connections.allow_from_any_ipv4(
+            port_range=aws_cdk.aws_ec2.Port.tcp(5555),
+            description="Port 5555 for inbound traffic",
+        )
         # self.create_selenium_hub()
         # self.create_chrome_node()
         # self.create_firefox_node()
 
         self.selenium_hub = self.create_fargate_service(
-            container_image=container_image,
-            container_name=f'{self.name}-container',
+            name='selenium-hub',
+            container_image='selenium/hub:3.141.59',
             port=4444,
             environment={
                 "GRID_BROWSER_TIMEOUT": "200000",
                 "GRID_TIMEOUT": "180",
                 "SE_OPTS": "-debug"
             }
+        )
+        self.chrome_hub = self.create_fargate_service(
+            name='selenium-chrome-node',
+            container_image='selenium/node-chrome:3.141.59',
+            port=4444,
+            environment=self.get_node_environment_variables(),
+            command=self.get_node_commands(),
+            entry_point=self.get_entry_point_commands(),
+        )
+        self.firefox_hub = self.create_fargate_service(
+            name='selenium-firefox-node',
+            container_image='selenium/node-firefox:3.141.59',
+            port=4444,
+            environment=self.get_node_environment_variables(),
+            command=self.get_node_commands(),
+            entry_point=self.get_entry_point_commands(),
         )
         self.selenium_hub.service.apply_removal_policy(
             aws_cdk.RemovalPolicy.DESTROY
@@ -45,7 +69,25 @@ class SeleniumTestService(well_architected_stacks.well_architected_stack.Stack):
             target_utilization_percent=70
         )
 
+    @staticmethod
+    def get_entry_point_commands():
+        return ["sh", "-c"]
 
+    @staticmethod
+    def get_node_commands():
+        return [
+            "PRIVATE=$(curl -s http://169.254.170.2/v2/metadata | jq -r '.Containers[0].Networks[0].IPv4Addresses[0]') ; export REMOTE_HOST=\"http://$PRIVATE:5555\"; export SE_OPTS=\"-host $PRIVATE -port 5555\" ; /opt/bin/entry_point.sh"
+        ]
+
+    def get_node_environment_variables(self):
+        return {
+            "HUB_PORT_4444_TCP_ADDR": self.selenium_hub.load_balancer.load_balancer_dns_name,
+            "HUB_PORT_4444_TCP_PORT": "4444",
+            "NODE_MAX_INSTANCES": "500",
+            "NODE_MAX_SESSION": "500",
+            "SE_OPTS": "-debug",
+            "shm_size": "512",
+        }
 
     @staticmethod
     def capacity_provider_strategies():
@@ -108,10 +150,17 @@ class SeleniumTestService(well_architected_stacks.well_architected_stack.Stack):
             )
         ]
 
+    @staticmethod
+    def create_log_driver(name):
+        return aws_cdk.aws_ecs.LogDriver.aws_logs(
+            stream_prefix=f'{name}-logs'
+        )
+
     def create_task_definition(
         self, name=None, container_image=None, container_name=None,
         port=None, cpu=None, memory=None,
-        environment=None,
+        environment=None, command=None,
+        entry_point=None,
     ):
         task_definition = aws_cdk.aws_ecs.FargateTaskDefinition(
             self, f'{name}FargateTaskDefinition',
@@ -123,20 +172,34 @@ class SeleniumTestService(well_architected_stacks.well_architected_stack.Stack):
         )
         task_definition.add_container(
             f'{name}Container',
-            image=aws_cdk.aws_ecs.ContainerImage.from_registry(container_image),
-            cpu=cpu,
-            memory_limit_mib=memory,
+            command=command,
             container_name=container_name,
-            essential=True,
+            cpu=cpu,
+            entry_point=entry_point,
             environment=environment,
+            essential=True,
+            image=aws_cdk.aws_ecs.ContainerImage.from_registry(container_image),
+            memory_limit_mib=memory,
             port_mappings=self.get_port_mappings(port),
-            logging=aws_cdk.aws_ecs.LogDriver.aws_logs(
-                stream_prefix=f'{name}-logs'
-            )
+            logging=self.create_log_driver(name)
         )
         return task_definition
 
-    def create_fargate_service(self, name=None, container_image=None, container_name=None, environment=None, port=None):
+    def create_application_load_balancer(self):
+        return
+
+    def create_a_fargate_service(self, name):
+        return aws_cdk.aws_ecs.FargateService(
+            self, f'{name}FargateService',
+            task_definition=None,
+            assign_public_ip=False,
+            platform_version=aws_cdk.aws_ecs.FargatePlatformVersion.LATEST,
+            cluster=self.ecs_cluster,
+            capacity_provider_strategies=self.capacity_provider_strategies(),
+            enable_execute_command=True
+        )
+
+    def create_fargate_service(self, name=None, container_image=None, environment=None, port=None, command=None, entry_point=None):
         return aws_cdk.aws_ecs_patterns.ApplicationLoadBalancedFargateService(
             self, f'{name}AlbEcsFargateService',
             assign_public_ip=False,
@@ -152,11 +215,13 @@ class SeleniumTestService(well_architected_stacks.well_architected_stack.Stack):
             ),
             task_definition=self.create_task_definition(
                 container_image=container_image,
-                container_name=container_name,
+                container_name=f'{name}-container',
                 port=port,
                 cpu=1024,
-                name=self.name,
+                name=name,
                 memory=2048,
                 environment=environment,
+                command=command,
+                entry_point=entry_point,
             ),
         )
