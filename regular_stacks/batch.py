5832517c057748bc68ee9f425ad838c867a7cf94
jadecobra <jakeitegsy@yahoo.com>
2022-10-06 20:24:33 -0500

    add batch

---
+++ regular_stacks/batch.py
@@ -0,0 +1,68 @@
+import aws_cdk
+import aws_cdk.aws_batch_alpha
+import constructs
+import well_architected_stacks
+
+
+class BatchEC2Stack(well_architected_stacks.well_architected_stack.Stack):
+
+    def __init__(self, scope: constructs.Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+        self.vpc = self.get_vpc()
+        batch_compute_environment = []
+        count = 3
+
+        # For loop to create Batch Compute Environments
+        for i in range(count):
+            batch_environment = aws_cdk.aws_batch_alpha.ComputeEnvironment(
+                self, f"BatchARM64Env{i}",
+                compute_resources=aws_cdk.aws_batch_alpha.ComputeResources(
+                type=aws_cdk.aws_batch_alpha.ComputeResourceType.SPOT,
+                bid_percentage=75,
+                instance_types=[
+                    aws_cdk.aws_ec2.InstanceType("a1.medium"),
+                    aws_cdk.aws_ec2.InstanceType("a1.large")
+                ],
+                image=aws_cdk.aws_ecs.EcsOptimizedImage.amazon_linux2(
+                    aws_cdk.aws_ecs.AmiHardwareType.ARM
+                ),
+                vpc_subnets=aws_cdk.aws_ec2.SubnetSelection(
+                    subnet_type=aws_cdk.aws_ec2.SubnetType.PRIVATE_WITH_NAT
+                ),
+                vpc=self.vpc
+                )
+            )
+
+            batch_compute_environment.append(
+                aws_cdk.aws_batch_alpha.JobQueueComputeEnvironment(
+                    compute_environment=batch_environment,
+                    order=i
+                )
+            )
+
+        self.batch_queue = aws_cdk.aws_batch_alpha.JobQueue(
+            self, "JobQueueArm64",
+            compute_environments=batch_compute_environment
+        )
+
+        self.batch_job_definition = aws_cdk.aws_batch_alpha.JobDefinition(
+            self, "MyJobDefArm64",
+            job_definition_name="CDKJobDefArm64",
+            container=aws_cdk.aws_batch_alpha.JobDefinitionContainer(
+                image=aws_cdk.aws_ecs.ContainerImage.from_registry(
+                    "public.ecr.aws/amazonlinux/amazonlinux:latest"
+                ),
+                command=["sleep", "60"],
+                memory_limit_mib=512,
+                vcpus=1
+            ),
+        )
+
+        aws_cdk.CfnOutput(self, "BatchJobQueue",value=self.batch_queue.job_queue_name)
+        aws_cdk.CfnOutput(self, "JobDefinition",value=self.batch_job_definition.job_definition_name)
+
+
+
+# app = App()
+# BatchEC2Stack(app, "BatchEC2Stack")
+# app.synth()
---
+++ regular_stacks/tests/test_regular_stacks.py
@@ -0,0 +1,26 @@
+import subprocess
+import jadecobra.toolkit
+
+
+class TestWellArchitectedPatterns(jadecobra.toolkit.TestCase):
+
+    @staticmethod
+    def patterns():
+        return (
+            'AutoscalingEcsService',
+            'AutoscalingEcsServiceWithPlacement',
+            'AutoscalingEcsCluster',
+            'AlbAutoscalingEcsService',
+            'NlbAutoscalingEcsService',
+            'AlbFargateService',
+            'NlbFargateService',
+            'NlbAutoscalingFargateService',
+            'SeleniumTestService',
+            'BatchEC2Stack'
+        )
+
+    def test_well_architected_cdk_patterns(self):
+        self.create_cdk_templates()
+        for pattern in self.patterns():
+            with self.subTest(i=pattern):
+                self.assert_cdk_templates_equal(pattern)
